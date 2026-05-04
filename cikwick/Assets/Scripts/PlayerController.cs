cf001a184bb84aa5cef68d0ab2fe99360d6af207
davutozden <davuttozdenn@gmail.com>
2026-05-05 02:45:50 +0300

    Zıplama Fonksiyonu Düzeltildi

--- cikwick/Assets/Scripts/PlayerController.cs
+++ cikwick/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1feff4b7eda4342b7afcbd44115de070a02b7503cd24291410b2381439f1ee0
-size 1728
+oid sha256:002b31b1b858982c58b3305b0ecdb71dacb35f584d83c47e6a0bbabd8a383bcf
+size 1959
